7a7bee6c6b3d2900a4855f2f62f0b99cab410fb9
Karlsson0214 <karlsson0214@gmail.com>
2020-09-20 18:37:17 +0200

    add .vscode/ to gitignore

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DemoRunner
 {
@@ -6,7 +7,60 @@ namespace DemoRunner
     {
         static void Main(string[] args)
         {
+            List<Runner> runners = new List<Runner>();
             Console.WriteLine("Välkommen");
+
+            PrintMenu();
+            string choice = Console.ReadLine();
+            while (choice != "a")
+            {
+                if (choice == "1")
+                {
+                    Console.WriteLine("Ange namn på löpare");
+                    String nameRunner = Console.ReadLine();
+
+                    Console.WriteLine("Ange distans i meter.");
+                    int distanceRunner;
+                    distanceRunner = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Ange tid i sekunder, decimaltal";
+                    double timeRunner;
+                    timeRunner = Convert.ToDouble(Console.ReadLine());
+
+                    Runner theRunner = new Runner();
+                    theRunner.name = nameRunner;
+                    theRunner.distance = distanceRunner;
+                    theRunner.time = timeRunner;
+
+                    runners.Add(theRunner);
+                }
+                else if (choice == "2")
+                {
+                    foreach(Runner myRunner in runners)
+                    {
+                        myRunner.Print();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("valet finns inte");
+                    PrintMenu();
+
+                }
+                PrintMenu();
+                choice = Console.ReadLine();
+            }
+
+
+        }
+
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("Välj ett alternativ.");
+            Console.WriteLine("1. mata in ett resultat");
+            Console.WriteLine("2 skriv ut resultat");
+            Console.WriteLine("a avsluta");
         }
     }
 }
--- Runner.cs
+++ Runner.cs
@@ -5,9 +5,17 @@ namespace DemoRunner
     class Runner
     {
         public String name;
-        public double seconds;
+        // time in seconds
+        public double time;
         // distance in meter
         public int distance; 
+
+
+        public void Print()
+        {
+            Console.WriteLine(name + " " + time + " sekunder, " 
+            + distance + "meter");
+        }
         
     }
 }
