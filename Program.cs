3c05873a7fd215ef26e5676bfbf019e057e84ba4
Karlsson0214 <karlsson0214@gmail.com>
2020-09-20 18:11:14 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
