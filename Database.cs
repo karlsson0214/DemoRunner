61102e7bd686561323ef4a1e0bd431c3b55c7b78
Karlsson0214 <karlsson0214@gmail.com>
2020-09-25 10:42:07 +0200

    Add runners and print runners

--- Database.cs
+++ Database.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
+
 
 namespace DemoRunner
 {
     class Database
     {
+        List<Runner> runners = new List<Runner>();
+        public void AddRunner(Runner myRunner)
+        {
+            runners.Add(myRunner);
+        }
 
+        public void PrintRunners()
+        {
+            foreach(Runner myRunner in runners)
+            {
+                myRunner.Print();
+            }
+        }
     }
+
+    
 }
