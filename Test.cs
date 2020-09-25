5ee2a6bfb66aff22430d1145041d5f82bceb9ed8
Karlsson0214 <karlsson0214@gmail.com>
2020-09-25 10:56:25 +0200

    add to runners and print

---
+++ Test.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace DemoRunner
+{
+    class Test
+    {
+        public void Run()
+        {
+            Database db = new Database();
+
+            Runner myRunner = new Runner();
+            myRunner.name = "Ada";
+            myRunner.distance = 100;
+            myRunner.time = 15.3;
+
+            db.AddRunner(myRunner);
+            
+            Runner myRunnerTwo = new Runner();
+            myRunnerTwo.name = "Babbage";
+            myRunnerTwo.distance = 400;
+            myRunnerTwo.time = 73.9;
+
+            db.AddRunner(myRunner);
+
+            db.PrintRunners();
+        }
+    }
+}
